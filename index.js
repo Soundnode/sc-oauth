2994977f88db5570a1342bf78762d142b39d6ed2
Michael Lancaster <michaellancaster@MacBook-Pro-de-Michael-Lancaster.local>
2014-04-29 11:17:44 -0500

    add view folder and enable access as static

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express')
     , app = express()
     , port = process.env.PORT || 3700;
 
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/views')); // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                     // log every request to the console
 app.use(bodyParser());                      // pull information from html in POST
 app.use(methodOverride());                  // simulate DELETE and PUT
